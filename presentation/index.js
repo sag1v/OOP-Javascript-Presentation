7b19ad658a794a1a34eabfc616b12139e94c8e41
sag1v <sagiv.bengiat@gmail.com>
2019-04-07 08:15:15 +0300

    this flow chart

--- presentation/index.js
+++ presentation/index.js
@@ -23,6 +23,7 @@ import {
   SubclassClass
 } from './components/Slides';
 
+import jsLogo from '../assets/jslogo.png';
 import thisRulesImage from '../assets/this-rules.png';
 
 // Import theme
@@ -62,92 +63,104 @@ const ClosingSlide = ({ title, children }) => (
   </Slide>
 );
 
+const Signature = styled(Flex)`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  color: #fff;
+  z-index: 99999;
+  font-size: 1.6em;
+`;
+
 export default class Presentation extends React.Component {
 
   render() {
     return (
-      <Deck textColor="#fff" bgColor="#1F2022" progress="pacman" transition={["slide"]} transitionDuration={650} theme={theme}>
-        <Slide bgColor="#1F2022">
-          <StyledHeading>JavaScript OOP</StyledHeading>
-          <Image height="250px" src="http://www.sclance.com/pngs/javascript-logo-png/javascript_logo_png_728629.png" />
-        </Slide>
-        <OpeningSlide title="Creating Objects">
-          <StyledHeading bold caps textColor="#1F2022">{`{ }`}</StyledHeading>
-        </OpeningSlide>
-        <CreatingObjects createUsing="literal" />
-        <CreatingObjects createUsing="dotNotation" />
-        <CreatingObjects createUsing="objectCreate" />
-        <CreatingObjects createUsing="functionFactory" />
-        <OpeningSlide title="let's run it" />
-        <CreatingObjects runCode />
-        <ClosingSlide title="d  r  y">
-          <Flex>Dont Repeat Yourself</Flex>
-        </ClosingSlide>
-        <OpeningSlide title="the [[prototype]] chain">
-          <Flex>We actually mean __proto__</Flex>
-        </OpeningSlide>
-        <Prototype />
-        <ClosingSlide title="really??">
-          <Flex>Can't we automate this?</Flex>
-        </ClosingSlide>
-        <OpeningSlide title="new">
-          <Flex>The new key word</Flex>
-        </OpeningSlide>
-        <NewFunc />
-        <OpeningSlide title="But first">
-          <Flex>What is a function really?</Flex>
-        </OpeningSlide>
-        <FunctionObjectCombo />
-        <ClosingSlide title="function">
-          <Flex>It's really a function + object combo</Flex>
-        </ClosingSlide>
-        <OpeningSlide title="invoking with new">
-          <Flex>a.k.a The constructor function pattern</Flex>
-        </OpeningSlide>
-        <NewFunc runCode />
-        <ClosingSlide title="nice">
-          <Flex>But can we do better?</Flex>
-        </ClosingSlide>
-        <OpeningSlide title="class">
-          <Flex>some sugar and stuff...</Flex>
-          <Flex>since ES2015</Flex>
-        </OpeningSlide>
-        <ClassFunc />
-        <ClosingSlide title="class">
-          <Flex>We will get back to it...</Flex>
-        </ClosingSlide>
-        <OpeningSlide title="[[prototype]]">
-          <Flex>Let's walk up the chain</Flex>
-        </OpeningSlide>
-        <NativeChain />
-        <NativeChain showSubClassing />
-        <ClosingSlide title="[[prototype]]">
-          <Flex>We can add links to the chain </Flex>
-          <Flex>usually called "sub-classing" </Flex>
-        </ClosingSlide>
-        <OpeningSlide title="this">
-          <Flex>The golden rules</Flex>
-        </OpeningSlide>
-        <Slide>
-          <StyledHeading>what is this?</StyledHeading>
-          <Image src={thisRulesImage}/>
-        </Slide>
-        <OpeningSlide title="sub class">
-          <Flex>Not the inheritance that you think</Flex>
-        </OpeningSlide>
-        <ClosingSlide title="sub class">
-          <Flex>factory functions</Flex>
-        </ClosingSlide>
-        <SubclassFactory />
-        <ClosingSlide title="sub class">
-          <Flex>constructor functions</Flex>
-        </ClosingSlide>
-        <SubclassConstructor />
-        <ClosingSlide title="sub class">
-          <Flex>class functions</Flex>
-        </ClosingSlide>
-        <SubclassClass />
-      </Deck>
+      <Flex>
+        <Deck textColor="#fff" bgColor="#1F2022" progress="pacman" transition={["slide"]} transitionDuration={650} theme={theme}>
+          <Slide bgColor="#1F2022">
+            <StyledHeading>JavaScript OOP</StyledHeading>
+            <Image height="250px" src={jsLogo} />
+          </Slide>
+          <OpeningSlide title="Creating Objects">
+            <StyledHeading bold caps textColor="#1F2022">{`{ }`}</StyledHeading>
+          </OpeningSlide>
+          <CreatingObjects createUsing="literal" />
+          <CreatingObjects createUsing="dotNotation" />
+          <CreatingObjects createUsing="objectCreate" />
+          <CreatingObjects createUsing="functionFactory" />
+          <OpeningSlide title="let's run it" />
+          <CreatingObjects runCode />
+          <ClosingSlide title="d  r  y">
+            <Flex>Dont Repeat Yourself</Flex>
+          </ClosingSlide>
+          <OpeningSlide title="the [[prototype]] chain">
+            <Flex>We actually mean __proto__</Flex>
+          </OpeningSlide>
+          <Prototype />
+          <ClosingSlide title="really??">
+            <Flex>Can't we automate this?</Flex>
+          </ClosingSlide>
+          <OpeningSlide title="new">
+            <Flex>The new key word</Flex>
+          </OpeningSlide>
+          <NewFunc />
+          <OpeningSlide title="But first">
+            <Flex>What is a function really?</Flex>
+          </OpeningSlide>
+          <FunctionObjectCombo />
+          <ClosingSlide title="function">
+            <Flex>It's really a function + object combo</Flex>
+          </ClosingSlide>
+          <OpeningSlide title="invoking with new">
+            <Flex>a.k.a The constructor function pattern</Flex>
+          </OpeningSlide>
+          <NewFunc runCode />
+          <ClosingSlide title="nice">
+            <Flex>But can we do better?</Flex>
+          </ClosingSlide>
+          <OpeningSlide title="class">
+            <Flex>some sugar and stuff...</Flex>
+            <Flex>since ES2015</Flex>
+          </OpeningSlide>
+          <ClassFunc />
+          <ClosingSlide title="class">
+            <Flex>We will get back to it...</Flex>
+          </ClosingSlide>
+          <OpeningSlide title="[[prototype]]">
+            <Flex>Let's walk up the chain</Flex>
+          </OpeningSlide>
+          <NativeChain />
+          <NativeChain showSubClassing />
+          <ClosingSlide title="[[prototype]]">
+            <Flex>We can add links to the chain </Flex>
+            <Flex>usually called "sub-classing" </Flex>
+          </ClosingSlide>
+          <OpeningSlide title="this">
+            <Flex>The golden rules</Flex>
+          </OpeningSlide>
+          <Slide>
+            <StyledHeading>what is this?</StyledHeading>
+            <Image src={thisRulesImage} />
+          </Slide>
+          <OpeningSlide title="sub class">
+            <Flex>Not the inheritance that you think</Flex>
+          </OpeningSlide>
+          <ClosingSlide title="sub class">
+            <Flex>factory functions</Flex>
+          </ClosingSlide>
+          <SubclassFactory />
+          <ClosingSlide title="sub class">
+            <Flex>constructor functions</Flex>
+          </ClosingSlide>
+          <SubclassConstructor />
+          <ClosingSlide title="sub class">
+            <Flex>class functions</Flex>
+          </ClosingSlide>
+          <SubclassClass />
+        </Deck>
+        <Signature>@sag1v</Signature>
+      </Flex>
     );
   }
 }
